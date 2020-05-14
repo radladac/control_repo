a322add8efb07c4d85f37d80e3ead3e0af1985b0
radladac <42018881+radladac@users.noreply.github.com>
2020-05-13 22:07:38 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,3 +6,10 @@ node default{
 node 'master.puppet.vm' {
   include role::master_server
 }
+node /^web/ {    ##matches anything beginning with web
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
+}
